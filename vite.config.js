acf8572a8cef8fcfd0e3c53d2bc054ee7dce1622
mehtatanay04 <mehtatanay40@gmail.com>
2025-09-24 01:56:24 +0530

    UI Changes

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      manifest: {
+        name: 'LitBuddy - Your Reading Companion',
+        short_name: 'LitBuddy',
+        description: 'A fun literacy app for children',
+        theme_color: '#0ea5e9',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     host: true,
     port: 3000
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
   }
 })
